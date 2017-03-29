336bfcc64348ad5020efca25f6f29843abb686f4
Gurcan Gulec <gurcanglc@gmail.com>
2017-03-29 13:45:37 +0300

    Fixing fps issue with changing the fps value in config.lua and making some design changes

--- config.lua
+++ config.lua
@@ -6,7 +6,7 @@ application = {
       width = aspectRatio > 1.5 and 320 or math.floor( 480 / aspectRatio ),
       height = aspectRatio < 1.5 and 480 or math.floor( 320 * aspectRatio ),
       scale = "letterBox",
-      fps = 30,
+      fps = 60,
  
       imageSuffix = {
          ["@2x"] = 1.5,
